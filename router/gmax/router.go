9032b9576ac24e0c1aa650d631bf0960e4609233
Anatolii Kartashov <anatoliy.kartashov@pashapay.az>
2023-01-08 13:54:30 +0300

    add templates and assets. TODO: include in build this files

--- router/gmax/router.go
+++ router/gmax/router.go
@@ -3,6 +3,7 @@ package gmax
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -16,8 +17,9 @@ var (
 )
 
 type APIServer struct {
-	r   *mux.Router
-	srv *http.Server
+	r    *mux.Router
+	srv  *http.Server
+	tmpl *template.Template
 }
 
 func init() {
@@ -36,12 +38,24 @@ func init() {
 }
 
 func (s *APIServer) StartServer() {
+	var err error
 	s.r.HandleFunc("/api/health", apiHealth)
 
 	// serve static files
 	// This will serve files under http://localhost:8000/static/<filename>
 	// hardcoded for security reasons
 	s.r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./web/assets"))))
+
+	// templates
+	// template.Must throw an error and exit
+	// tpl = template.Must(template.ParseGlob("templates/*.html"))
+	// use template tmpl.Execute(w, data) and useage in template: {{.PageTitle}} would get value from data.PageTitle
+	s.tmpl, err = template.ParseGlob("web/templates/*")
+
+	if err != nil {
+		log.Panic("Error: ", err)
+	}
+	log.Println(s.tmpl.DefinedTemplates())
 
 	// make connection string
 	s_addr := fmt.Sprintf("%s:%s", viper.GetString("server.host"), viper.GetString("server.port"))
@@ -51,8 +65,8 @@ func (s *APIServer) StartServer() {
 		Handler: S.r,
 		Addr:    s_addr,
 		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		WriteTimeout: time.Duration(viper.GetInt("server.timeout.write")) * time.Second,
+		ReadTimeout:  time.Duration(viper.GetInt("server.timeout.read")) * time.Second,
 	}
 
 	log.Fatal(s.srv.ListenAndServe())
